fef0f3887c3f7e5fc61a1b5cbd590e011205e659
OAT <64010860@kmitl.ac.th>
2021-10-01 14:01:40 +0700

    Add project files.

---
+++ Problem65/65method1.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+
+//65.จงเขียนโปรแกรมเพื่อรับ String 1 ชุดแล้วแสดงผลลัพธ์เป็นสามเหลี่ยมจาก string ที่รับเข้ามา ดังตัวอย่าง (Level 3)
+//Input :  asdf
+//Output : asdf
+//         asd
+//         as
+//         a
+
+
+
+
+int main()
+{
+	int i,length;
+	char sen[1000];
+	printf("Input : ");
+	scanf("%s", sen);
+	length = strlen(sen);
+	printf("Output : ");
+	while (length != 0)
+	{
+		for (i = 0; i < length; i++)
+		{
+			printf("%c", sen[i]);
+		}
+		printf("\n");
+		printf("       	 ");
+		length--;
+	}
+
+
+	
+
+
+}
---
+++ Problem65/65method2.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+
+
+//65.จงเขียนโปรแกรมเพื่อรับ String 1 ชุดแล้วแสดงผลลัพธ์เป็นสามเหลี่ยมจาก string ที่รับเข้ามา ดังตัวอย่าง (Level 3)
+//Input :  asdf
+//Output : asdf
+//         asd
+//         as
+//         a
+
+/*int main()
+{
+	int i=0,len;
+	char sen[1000];
+	printf("Input : ");
+	scanf("%s", sen);
+	len = strlen(sen);
+	printf("Output : ");
+	while (sen[i] != '\0' && len!=0)
+	{
+		printf("%c", sen[i]);
+		i++;
+		if (i == len)
+		{
+			sen[i] = '\0';
+			len--;
+			i = 0;
+			printf("\n");
+			printf("       	 ");
+		}
+	}
+}*/
